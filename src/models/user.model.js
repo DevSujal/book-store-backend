5d73b442d4bd9527acd2c6a3d0e7f71b53a1dd8f
DevSujal <nimjesujal2004@gmail.com>
2024-05-30 14:46:04 +0530

    "Added bcryptjs and jsonwebtoken dependencies, updated user.model.js to include password hashing and JWT token generation"

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 
 const userSchema = new mongoose.Schema({
@@ -29,5 +31,36 @@ const userSchema = new mongoose.Schema({
         type : String
     }
 },{timestamps : true})
+
+userSchema.pre("save", async function (next) {
+
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async (password) => (
+    await bcrypt.compare(password, this.password)
+)
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign({
+        _id : this._id,
+    },process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign({
+        _id : this._id,
+        username : this.username,
+        fullname : this.fullname,
+    },process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
 
 export const User = mongoose.model("User", userSchema)
